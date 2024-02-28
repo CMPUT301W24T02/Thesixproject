24432091a08c2e87b7f02b47bdc63f2612c2cb6f
David Lee <daviddaviddavid807@gmail.com>
2024-02-28 14:36:18 -0700

    <add>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,8 @@ android {
 
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
+    implementation ("androidmads.library.qrgenearator:QRGenearator:1.0.3")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
