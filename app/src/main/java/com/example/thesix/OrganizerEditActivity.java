0bf020abef6f392c052a355271b463acd23e3742
Arjun <arjun10@ualberta.ca>
2024-03-03 16:57:43 -0700

    Minor changes in constraints of event_create_edit_screen layout

--- app/src/main/java/com/example/thesix/OrganizerEditActivity.java
+++ app/src/main/java/com/example/thesix/OrganizerEditActivity.java
@@ -3,6 +3,8 @@ package com.example.thesix;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +12,9 @@ public class OrganizerEditActivity extends AppCompatActivity {
     private Button saveChangeButton;
     private Button shareInviteButton;
     private Button sharePromoButton;
+    private Button backButton;
+    private EditText eventDescription;
+    private ImageView eventPoster;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,9 @@ public class OrganizerEditActivity extends AppCompatActivity {
         saveChangeButton = findViewById(R.id.saveChangesButton);
         shareInviteButton = findViewById(R.id.shareInviteButton);
         sharePromoButton = findViewById(R.id.sharePromoButton);
+        backButton = findViewById(R.id.backButton);
+        eventDescription = findViewById(R.id.eventDescription);
+        eventPoster = findViewById(R.id.eventPoster);
         saveChangeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,6 +40,23 @@ public class OrganizerEditActivity extends AppCompatActivity {
             }
         });
         shareInviteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //Code to be added for where back button takes us
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        String eventDes = eventDescription.getText().toString(); //Getting event description from user and storing it as a string
+
+        eventPoster.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
