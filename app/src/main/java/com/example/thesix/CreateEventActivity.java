6e14bef8ba539f8d074104ec133884fd0d79e02c
Arjun <arjun10@ualberta.ca>
2024-03-06 15:24:52 -0700

    Moved code from Organizer edit activity to create Event activity

--- app/src/main/java/com/example/thesix/CreateEventActivity.java
+++ app/src/main/java/com/example/thesix/CreateEventActivity.java
@@ -1,4 +1,92 @@
 package com.example.thesix;
 
-public class CreateEventActivity {
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateEventActivity extends AppCompatActivity {
+    private Button saveChangeButton;
+    private Button shareInviteButton;
+    private Button sharePromoButton;
+    private Button backButton;
+    private EditText eventDescription;
+    private ImageView eventPoster;
+    private Uri imageuri;
+    private EventDetailsDB firestoreHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_event);
+
+        saveChangeButton = findViewById(R.id.saveChangesButton);
+        shareInviteButton = findViewById(R.id.shareInviteButton);
+        sharePromoButton = findViewById(R.id.sharePromoButton);
+        backButton = findViewById(R.id.backButton);
+        eventDescription = findViewById(R.id.eventDescription);
+        eventPoster = findViewById(R.id.eventPoster);
+        String eventDes = "Arjun"; //Getting event description from user and storing it as a string
+
+        //Adds event details to database
+        saveChangeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firestoreHelper.saveEventDescription(eventDes);
+            }
+        });
+        sharePromoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        shareInviteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //Code to be added for where back button takes us
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        eventPoster.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choosePicture(); //Function to select images
+            }
+        });
+
+
+    }
+    // Function to select image from the storage
+    private void choosePicture() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1 && resultCode==RESULT_OK && data!=null && data.getData()!=null)
+        {
+            imageuri=data.getData();
+            eventPoster.setImageURI(imageuri);
+        }
+    }
 }
