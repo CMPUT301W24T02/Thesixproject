95c70b32b6bb1b18d207e57f0a383e4d1485de3d
Arjun <arjun10@ualberta.ca>
2024-03-07 13:18:21 -0700

    Added classes for admin

---
+++ app/src/main/java/com/example/thesix/AdminActivity.java
@@ -0,0 +1,45 @@
+package com.example.thesix;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminActivity extends AppCompatActivity {
+    private Button viewEvents;
+    private Button viewProfiles;
+    private Button viewImages;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_mainactivity);
+
+        viewEvents = findViewById(R.id.viewEvents);
+        viewProfiles = findViewById(R.id.viewProfiles);
+        viewImages = findViewById(R.id.viewImages);
+
+        viewEvents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(AdminActivity.this, OrganizerCreateActivity.class));
+            }
+        });
+
+        viewEvents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+
+        viewImages.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+    }
+}
