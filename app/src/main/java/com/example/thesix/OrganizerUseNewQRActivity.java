de411705611abbed775eb808e68e75e37311f1fc
Arjun <arjun10@ualberta.ca>
2024-03-06 19:09:43 -0700

    Add event description and picture stuff

--- app/src/main/java/com/example/thesix/OrganizerUseNewQRActivity.java
+++ app/src/main/java/com/example/thesix/OrganizerUseNewQRActivity.java
@@ -2,6 +2,7 @@ package com.example.thesix;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Base64;
@@ -10,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -33,12 +35,14 @@ import java.util.List;
 public class OrganizerUseNewQRActivity extends AppCompatActivity {
     private EditText descriptionEditText;
     private Button createEventButton;
-    private ImageView qrCodeImageView;
     private QrCodeDB firestoreHelper;
     private Button backButton;
 
+    private ImageView eventPoster;
+    private Uri imageuri;
 
     public Long count;
+
     @Override
 
 
@@ -47,64 +51,56 @@ public class OrganizerUseNewQRActivity extends AppCompatActivity {
         setContentView(R.layout.organizer_use_new_qr_screen);
         descriptionEditText = findViewById(R.id.editTextText);
         createEventButton = findViewById(R.id.createEventButton);
-        qrCodeImageView = findViewById(R.id.qrCodeImageView);
         firestoreHelper = new QrCodeDB();
         backButton = findViewById(R.id.backButton);
+        eventPoster = findViewById(R.id.eventPoster);
         String deviceID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
 
 
-
+        eventPoster.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choosePicture(); //Function to select images
+            }
+        });
         createEventButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                readData(new MyCallback() {
+                    @Override
+                    public void onCallback(long num) {
+                        Log.d("callback", String.valueOf(num));
+                        try {
+                            String deviceID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID); //get device ID
+                            String description = descriptionEditText.getText().toString();
+                            String qrString = deviceID + description + num;
+                            QRCodeWriter writer = new QRCodeWriter();
+                            BitMatrix bitMatrix = writer.encode(qrString, BarcodeFormat.QR_CODE, 512, 512);
+                            Bitmap bitmap = Bitmap.createBitmap(512, 512, Bitmap.Config.RGB_565);
 
-
-                    readData(new MyCallback() {
-                        @Override
-                        public void onCallback(long num) {
-                            Log.d("callback", String.valueOf(num));
-                            try {
-                                String deviceID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID); //get device ID
-                                String description = descriptionEditText.getText().toString();
-                                String qrString = deviceID + description + num;
-                                QRCodeWriter writer = new QRCodeWriter();
-                                BitMatrix bitMatrix = writer.encode(qrString, BarcodeFormat.QR_CODE, 512, 512);
-                                Bitmap bitmap = Bitmap.createBitmap(512, 512, Bitmap.Config.RGB_565);
-
-                                for (int x = 0; x < 512; x++) {
-                                    for (int y = 0; y < 512; y++) {
-                                        bitmap.setPixel(x, y, bitMatrix.get(x, y) ? getResources().getColor(R.color.black) : getResources().getColor(R.color.white));
-                                    }
+                            for (int x = 0; x < 512; x++) {
+                                for (int y = 0; y < 512; y++) {
+                                    bitmap.setPixel(x, y, bitMatrix.get(x, y) ? getResources().getColor(R.color.black) : getResources().getColor(R.color.white));
                                 }
-
-
-                                // Display QR Code
-                                qrCodeImageView.setImageBitmap(bitmap);
-
-
-                                // Convert Bitmap to Base64 String
-                                String qrImageBase64 = bitmapToBase64(bitmap);
-
-
-                                // Save invite QR Code in Firestore
-                                MyQRCode qrCode = new MyQRCode(qrImageBase64,num,description);
-                                firestoreHelper.saveInviteQRCode(deviceID, qrCode);
-                            }
-                            catch (WriterException e) {
-                                Log.e("MainActivity", "Error generating QR code", e);
                             }
 
+
+                            // Convert Bitmap to Base64 String
+                            String qrImageBase64 = bitmapToBase64(bitmap);
+
+
+                            // Save invite QR Code in Firestore
+                            MyQRCode qrCode = new MyQRCode(qrImageBase64, num, description);
+                            firestoreHelper.saveInviteQRCode(deviceID, qrCode);
+                        } catch (WriterException e) {
+                            Log.e("MainActivity", "Error generating QR code", e);
                         }
-                    });
 
-
-
-
+                    }
+                });
+                Toast.makeText(OrganizerUseNewQRActivity.this, "Your Event has been created.", Toast.LENGTH_SHORT).show();
             }
         });
-
-
-
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -114,11 +110,12 @@ public class OrganizerUseNewQRActivity extends AppCompatActivity {
         });
 
 
-
     }
+
     public interface MyCallback {
         void onCallback(long num);
     }
+
     public void readData(MyCallback myCallback) {
         String deviceID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
         firestoreHelper.getDocRef(deviceID).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
@@ -141,11 +138,29 @@ public class OrganizerUseNewQRActivity extends AppCompatActivity {
         });
 
     }
+
     private String bitmapToBase64(Bitmap bitmap) {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
         byte[] byteArray = byteArrayOutputStream.toByteArray();
         return Base64.encodeToString(byteArray, Base64.DEFAULT);
+    }
+
+    // Function to select image from the storage
+    private void choosePicture() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imageuri = data.getData();
+            eventPoster.setImageURI(imageuri);
+        }
     }
 }
 
