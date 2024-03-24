6f082a0a840798ebe53e64c5b563b673f2a3505a
jingru6 <jingru6@ualberta.ca>
2024-03-23 21:28:13 -0600

    Update Attendee class and attendee profile layout
    Update Attendee class and attendee profile layout

--- app/src/main/java/com/example/thesix/Attendee.java
+++ app/src/main/java/com/example/thesix/Attendee.java
@@ -14,26 +14,43 @@ public class Attendee {
     */
     private String name;
     private Long checkin;
+    private String contact;
+    private String homePage;
+
+    /**
+     * Constructor for Attendee with contact information.
+     * This constructor is used when you have the attendee's name,
+     * contact number, and home page details available. It initializes
+     * the object with these three pieces of information.
+     *
+     * @param name      the name of the attendee
+     * @param contact   the contact number of the attendee
+     * @param homePage  the home page URL or handle of the attendee
+     */
+    public Attendee(String name,String contact, String homePage){
+
+        this.name = name;
+        this.contact = contact;
+        this.homePage = homePage;
+    }
 
     /**
      * Saves attendee information
      * @param : Attendee attendee
      * @return : void
      */
+    public Attendee(String name, Long checkin) {
 
-    public Attendee(String name, Long checkin){
-        // Temporary Constructor
         this.name = name;
         this.checkin = checkin;
     }
+
 
     /**
      * gets profile Name
      * @param :
      * @return : String
      */
-
-
     public String getName() {
         return name;
     }
@@ -43,8 +60,6 @@ public class Attendee {
      * @param :
      * @return : String
      */
-
-
     public void setName(String name) {
         this.name = name;
     }
@@ -54,7 +69,6 @@ public class Attendee {
      * @param :
      * @return : Long
      */
-
     public Long getCheckin() {
         return checkin;
     }
@@ -64,10 +78,48 @@ public class Attendee {
      * @param : Long checkin
      * @return : void
      */
-
     public void setCheckin(Long checkin) {
         this.checkin = checkin;
     }
 
+    /**
+     * Gets the contact number of the attendee.
+     * This method retrieves the contact number that was previously set for this attendee.
+     *
+     * @return A string representing the contact number of the attendee.
+     */
+    public String getContact() {
+        return contact;
+    }
+
+    /**
+     * Sets the contact number of the attendee.
+     * This method allows updating the contact number for this attendee.
+     *
+     * @param contact A string representing the new contact number to be set for the attendee.
+     */
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    /**
+     * Gets the home page of the attendee.
+     * This method retrieves the home page URL or handle that was previously set for this attendee.
+     *
+     * @return A string representing the home page of the attendee.
+     */
+    public String getHomePage() {
+        return homePage;
+    }
+
+    /**
+     * Sets the home page of the attendee.
+     * This method allows updating the home page information for this attendee.
+     *
+     * @param homePage A string representing the new home page URL or handle to be set for the attendee.
+     */
+    public void setHomePage(String homePage) {
+        this.homePage = homePage;
+    }
 
 }
