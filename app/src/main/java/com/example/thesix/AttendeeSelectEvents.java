d84da278b111fbb399823c31f0a3f89eaa4ffcfa
Arjun <arjun10@ualberta.ca>
2024-03-31 00:00:44 -0600

    Added attendee page to select different events to view

---
+++ app/src/main/java/com/example/thesix/AttendeeSelectEvents.java
@@ -0,0 +1,36 @@
+package com.example.thesix;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AttendeeSelectEvents extends AppCompatActivity {
+    private Button allEvents;
+    private Button signedupEvents;
+    private Button checkedinEvents;
+    private Button backbtn;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.attendee_select_events);
+        allEvents = findViewById(R.id.allEvents);
+        signedupEvents = findViewById(R.id.signedUpEvents);
+        checkedinEvents = findViewById(R.id.checkedInEvents);
+        backbtn = findViewById(R.id.backButton);
+
+
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AttendeeSelectEvents.this, AttendeeMainActivity.class));
+            }
+        });
+
+    }
+}
+
--- app/src/main/java/com/example/thesix/MainActivity.java
+++ app/src/main/java/com/example/thesix/MainActivity.java
@@ -61,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
             //startActivity(new Intent(MainActivity.this, OrganizerMainActivity.class));
             startActivity(new Intent(MainActivity.this, AdminActivity.class));
             //startActivity(new Intent(MainActivity.this, AttendeeProfileActivity.class));
+            startActivity(new Intent(MainActivity.this, AttendeeMainActivity.class));
         }
     }
 
