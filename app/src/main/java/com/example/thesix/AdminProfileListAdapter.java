665bf091571108393c0b660c052342a9bc41df9b
D-e-elle <84299963+D-e-elle@users.noreply.github.com>
2024-04-01 12:53:18 -0600

    Changing to Object

--- app/src/main/java/com/example/thesix/AdminProfileListAdapter.java
+++ app/src/main/java/com/example/thesix/AdminProfileListAdapter.java
@@ -2,37 +2,47 @@ package com.example.thesix;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
-public class AdminProfileListAdapter extends ArrayAdapter<Bitmap> {
+public class AdminProfileListAdapter extends ArrayAdapter<Attendee> {
 
     private Context context;
-    private ArrayList<Bitmap> image;
+    private ArrayList<Attendee> attendees;
 
-    public AdminProfileListAdapter(Context context, ArrayList<Bitmap> data) {
+    public AdminProfileListAdapter(Context context, ArrayList<Attendee> data) {
         super(context, R.layout.image_list_content, data);
         this.context = context;
-        this.image = data;
+        this.attendees = attendees;
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(R.layout.image_list_content, parent, false);
-        }
+        View view = convertView;
 
-        ImageView imageView = convertView.findViewById(R.id.image_list);
-        imageView.setImageBitmap(image.get(position));
+        if (view == null) {
+            Log.d("DD", "inflate the image list content");
+            convertView = LayoutInflater.from(context).inflate(R.layout.profile_list_content, parent, false);
+        }
+        Log.d("DD", "attendees.get(position) is next");
+        Attendee attendee = attendees.get(position);
+        TextView attendeeName = view.findViewById(R.id.profile_text);
+
+        attendeeName.setText(attendee.getName());
+
+        //ImageView imageView = convertView.findViewById(R.id.image_list);
+        //imageView.setImageBitmap(image.get(position));
 
         return convertView;
     }
