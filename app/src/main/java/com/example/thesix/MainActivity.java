ebafa429d374557efc452c5cfc563af8618e6bd7
AlexGuo7060 <158379323+AlexGuo7060@users.noreply.github.com>
2024-03-31 22:51:54 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/thesix/MainActivity.java
+++ app/src/main/java/com/example/thesix/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
             //startActivity(new Intent(MainActivity.this, AttendeeMainActivity.class));
             //startActivity(new Intent(MainActivity.this, OrganizerMainActivity.class));
-            //startActivity(new Intent(MainActivity.this, AdminActivity.class));
+            startActivity(new Intent(MainActivity.this, AdminActivity.class));
             //startActivity(new Intent(MainActivity.this, AttendeeProfileActivity.class));
             startActivity(new Intent(MainActivity.this, AttendeeMainActivity.class));
         }
