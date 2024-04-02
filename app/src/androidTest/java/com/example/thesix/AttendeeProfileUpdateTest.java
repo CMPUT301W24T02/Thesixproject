f37dee47e6c43d5b1f30d883bc7be63a88643db9
jingru6 <jingru6@ualberta.ca>
2024-04-01 23:20:58 -0600

    Update Attendee UI Test
    Update Attendee UI Test

--- app/src/androidTest/java/com/example/thesix/AttendeeProfileUpdateTest.java
+++ app/src/androidTest/java/com/example/thesix/AttendeeProfileUpdateTest.java
@@ -1,6 +1,7 @@
 package com.example.thesix;
 
 import android.app.Activity;
+import android.widget.EditText;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -8,6 +9,7 @@ import androidx.test.rule.ActivityTestRule;
 import com.robotium.solo.Solo;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Rule;
 
 import org.junit.Before;
@@ -45,6 +47,45 @@ public class AttendeeProfileUpdateTest {
         Activity activity = rule.getActivity();
 
         solo.sleep(3000);
+    }
+
+    /**
+     * check if profile information could be added properly
+     */
+    @Test
+    public void test_add_ProfileInfo(){
+        // Asserts that the current activity is the AttendeeProfileUpdate.Otherwise, show "Wrong Activity"
+        solo.assertCurrentActivity("Wrong Activity",AttendeeProfileUpdate.class);
+
+        // check if an attendee information can be added
+        test_add(solo);
+
+    }
+
+    /**
+     * check if an attendee information can be added
+     */
+    public void test_add(Solo solo){
+        String sample_Name = "Test Name";
+        String sample_Contact = "7806661192";
+        String sample_HomePage = "UA301";
+
+        //get a reference to editTexts
+        EditText EditText_name = (EditText) solo.getView(R.id.name_editText);
+        EditText EditText_contact = (EditText) solo.getView(R.id.contact_editText);
+        EditText EditText_homePage = (EditText) solo.getView(R.id.homePage_editText);
+
+        // Enter sample data named "Test Name" and Set text for all editTexts
+        solo.enterText(EditText_name,sample_Name);
+        solo.enterText(EditText_contact ,sample_Contact);
+        solo.enterText(EditText_homePage,sample_HomePage);
+
+        // Assert the result
+        Assert.assertEquals(sample_Name, EditText_name.getText().toString());
+        Assert.assertEquals(sample_Contact, EditText_contact.getText().toString());
+        Assert.assertEquals(sample_HomePage, EditText_homePage.getText().toString());
+
+        solo.sleep(6000);
     }
 
     /**
