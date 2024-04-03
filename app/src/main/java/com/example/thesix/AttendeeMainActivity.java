09f7b1ca70113f85c1e202d1c57109a4f590f052
Alex Guo <zg@ualberta.ca>
2024-04-03 12:55:24 -0600

    delete error line

--- app/src/main/java/com/example/thesix/AttendeeMainActivity.java
+++ app/src/main/java/com/example/thesix/AttendeeMainActivity.java
@@ -93,29 +93,7 @@ public class AttendeeMainActivity extends AppCompatActivity implements IbaseGpsL
         if (bundle != null) {
             deviceID = bundle.getString("deviceID");
         }
-
-//        FirebaseMessaging.getInstance().getToken()
-//                .addOnCompleteListener(new OnCompleteListener<String>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<String> task) {
-//                        if (!task.isSuccessful()) {
-////                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
-//                            return;
-//                        }
-//
-//                        // Get new FCM registration token
-//                        String token = task.getResult();
-//
-//                        // Log and toast
-////                        String msg = getString(R.string.msg_token_fmt, token);
-////                        Log.d(TAG, msg);
-////                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-
-        String token = PushNotificationService.getToken(this);
         String finalDeviceID = deviceID;
-        firestoreHelper.saveDeviceIDToToken(finalDeviceID,token);
         askNotificationPermission();
 
         scanButton.setOnClickListener(new View.OnClickListener() {
