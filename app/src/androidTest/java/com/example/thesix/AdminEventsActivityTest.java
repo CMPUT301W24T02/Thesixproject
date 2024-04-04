1001b15cfa3265630a3ee86d343ac147b24ae3fb
jingru6 <jingru6@ualberta.ca>
2024-04-03 20:00:28 -0600

    Add some UI Test for admin Activities
    Add some UI Test for admin Activities

---
+++ app/src/androidTest/java/com/example/thesix/AdminEventsActivityTest.java
@@ -0,0 +1,78 @@
+package com.example.thesix;
+
+import android.app.Activity;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Rule;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(AndroidJUnit4.class)
+public class AdminEventsActivityTest {
+    /*Declaration of variables*/
+
+    private Solo solo;
+
+    /*Establishes test rules*/
+    @Rule
+    public ActivityTestRule<AdminEventsActivity> rule =
+            new ActivityTestRule<>(AdminEventsActivity.class, true, true);
+
+    /**
+     * Runs before all tests and creates solo instance.
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception{
+
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
+    }
+    /**
+     * Gets the Activity
+     * @throws Exception
+     */
+    @Test
+    public void a_start() throws Exception{
+
+        Activity activity = rule.getActivity();
+
+        solo.sleep(3000);
+    }
+
+    /**
+     * Test if can switch back to AdminActivity from AdminEventsActivity by back Button
+     */
+    @Test
+    public void test_backButton(){
+
+        // Asserts that the current activity is the AdminEventsActivity.Otherwise, show "Wrong Activity"
+        solo.assertCurrentActivity("Wrong Activity", AdminEventsActivity.class);
+
+        // click goBack Button HERE
+        solo.clickOnView(solo.getView(R.id.backButton));
+
+        // Asserts that the current activity is the AdminActivity.Otherwise, show "Wrong Activity"
+        solo.assertCurrentActivity("Wrong Activity", AdminActivity.class);
+
+        solo.sleep(3000);
+
+    }
+
+
+    /**
+     * Close activity after each test
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception{
+        solo.finishOpenedActivities();
+    }
+}
