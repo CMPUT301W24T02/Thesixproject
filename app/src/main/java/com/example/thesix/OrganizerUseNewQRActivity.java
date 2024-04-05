21282f66fe9d6537649a762eb977cb033908ccdf
melritac <melritac@ualberta.ca>
2024-04-04 23:34:26 -0600

    some stuff to do with location

--- app/src/main/java/com/example/thesix/OrganizerUseNewQRActivity.java
+++ app/src/main/java/com/example/thesix/OrganizerUseNewQRActivity.java
@@ -155,8 +155,8 @@ public class OrganizerUseNewQRActivity extends AppCompatActivity {
                                 List<Long> checkIn = new ArrayList<Long>();
                                 List<String> attendeeIDList = new ArrayList<>();
                                 List<String> signUpIDList = new ArrayList<>();
-                                List<Location> locations = new ArrayList<>();
-                                EventDetails eventdetail = new EventDetails(eventImageBase64, inviteQrImageBase64, promoQrImageBase64, num, description, eventName,checkIn, 0L,attendeeIDList,signUpIDList,locations);
+                                List<Location> location = new ArrayList<>();
+                                EventDetails eventdetail = new EventDetails(eventImageBase64, inviteQrImageBase64, promoQrImageBase64, num, description, eventName,checkIn, 0L,attendeeIDList,signUpIDList,location);
                                 firestoreHelper.saveInviteQRCode(deviceID, eventdetail,num);
 
                             } catch (WriterException e) {
