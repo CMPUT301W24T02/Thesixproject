1055954323e5ad665d0eb4c4ce91d2a8323ce082
melritac <melritac@ualberta.ca>
2024-04-06 17:09:08 -0600

    javadocs

--- app/src/main/java/com/example/thesix/AttendeeProfileUpdate.java
+++ app/src/main/java/com/example/thesix/AttendeeProfileUpdate.java
@@ -30,8 +30,18 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
+/**
+ * This class represents an activity in the Android application responsible for updating attendee profiles.
+ * It allows users to input their name, contact information, homepage URL, and select a profile picture from the gallery.
+ * Attendee profile data is persisted using SharedPreferences, while profile pictures are stored internally.
+ * SHA-256 hashing is utilized for generating unique identifiers based on attendee information and device ID.
+ *
+ *
+ */
 public class AttendeeProfileUpdate extends AppCompatActivity {
+    // Attributes
     private EditText nameEditText, contactEditText, homePageEditText;
     private ImageView profileImageView;
     private Button submitButton, backButton, removePictureButton;
@@ -42,10 +52,18 @@ public class AttendeeProfileUpdate extends AppCompatActivity {
     List<Integer> colorlist;
     int[] finalColorList1;
     int[] finalColorList2;
+
+
+    /** Initializes the activity and its views.
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down then this Bundle contains the data it most
+     *                           recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.attendee_profile_update);
+        //colour lists
         finalColorList1 = new int[16];
         finalColorList2 = new int[16];
         colorlist = Arrays.asList(
@@ -59,8 +77,11 @@ public class AttendeeProfileUpdate extends AppCompatActivity {
                 getResources().getColor(R.color.orange),
                 getResources().getColor(R.color.purple),
                 getResources().getColor(R.color.red));
+
         // Initialize views
         deviceID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+
+        //initizaling buttons
         nameEditText = findViewById(R.id.name_editText);
         contactEditText = findViewById(R.id.contact_editText);
         homePageEditText = findViewById(R.id.homePage_editText);
@@ -72,7 +93,7 @@ public class AttendeeProfileUpdate extends AppCompatActivity {
         SharedPreferences prefs = getSharedPreferences("AttendeePrefs", MODE_PRIVATE);
         loadExistingData(prefs);
 
-
+        // Set a click listener for the profileImageView, which allows users to select a photo from the gallery
         profileImageView.setOnClickListener(v -> {
             // Intent to pick a photo from the gallery
             Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
@@ -152,10 +173,20 @@ public class AttendeeProfileUpdate extends AppCompatActivity {
         removePictureButton.setOnClickListener(v -> removeProfilePicture(prefs));
     }
 
+    /** On activity Result
+     * @param requestCode The integer request code originally supplied to
+     *                    startActivityForResult(), allowing you to identify who this
+     *                    result came from.
+     * @param resultCode  The integer result code returned by the child activity
+     *                    through its setResult().
+     * @param data        An Intent, which can return result data to the caller
+     *                    (various data can be attached to Intent "extras").
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 2 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            //get data uri selected images
             Uri selectedImage = data.getData();
             String imagePath = saveImageToInternalStorage(selectedImage);
             temporaryImagePath = imagePath; // Update the temporaryImagePath
@@ -163,56 +194,104 @@ public class AttendeeProfileUpdate extends AppCompatActivity {
         }
     }
 
+    /**
+     * Saves the attendee's profile data (name, contact, homepage) to the SharedPreferences.
+     *
+     * @param prefs The SharedPreferences instance to store the data.
+     */
     private void saveProfileData(SharedPreferences prefs) {
+        //// Get an editor to modify the SharedPreferences
         SharedPreferences.Editor editor = prefs.edit();
+        // Save the profile data to SharedPreferences
         editor.putString("name", nameEditText.getText().toString());
         editor.putString("contact", contactEditText.getText().toString());
         editor.putString("homePage", homePageEditText.getText().toString());
+        // Apply the changes
         editor.apply();
     }
-
+    /**
+     * Loads existing profile data from SharedPreferences and displays it in the corresponding EditText fields and ImageView.
+     *
+     * @param prefs The SharedPreferences instance from which to load the data.
+     */
     private void loadExistingData(SharedPreferences prefs) {
+        // Set the text in the EditText fields to the corresponding values stored in SharedPreferences
         nameEditText.setText(prefs.getString("name", ""));
         contactEditText.setText(prefs.getString("contact", ""));
         homePageEditText.setText(prefs.getString("homePage", ""));
+
+        // Get the profile image path from SharedPreferences
         String imagePath = prefs.getString("profileImagePath", null);
+        // If a profile image path exists, set the ImageView to display the image
         if (imagePath != null) {
             profileImageView.setImageURI(Uri.fromFile(new File(imagePath)));
         }
     }
 
+    /**
+     * Removes the profile picture by deleting its reference from SharedPreferences and resetting the ImageView to display a default image.
+     * If the profile picture is associated with a document ID, it also removes the profile image from the database.
+     *
+     * @param prefs The SharedPreferences instance from which to remove the profile picture reference.
+     */
     private void removeProfilePicture(SharedPreferences prefs) {
+        // Retrieve the document ID associated with the profile picture from SharedPreferences
         String documentId = prefs.getString("documentId", "");
+
+        // Check if a document ID exists
         if (!documentId.isEmpty()) {
+            // Initialize AttendeeDB instance to access database operations
             AttendeeDB attendeeDB = new AttendeeDB();
+            // Remove the profile image from the database using the document ID
             attendeeDB.removeProfileImage(documentId);
+            // Get SharedPreferences editor to remove the profile picture reference
             SharedPreferences.Editor editor = prefs.edit();
             editor.remove("profileImagePath");
             editor.apply();
+            // Reset the ImageView to display a default image
             profileImageView.setImageResource(android.R.drawable.ic_menu_gallery); // Reset to a default image
             Toast.makeText(this, "Profile picture removed", Toast.LENGTH_SHORT).show();
         } else {
+            // If no document ID is found, display a toast message indicating that no profile picture is present
             Toast.makeText(this, "No profile picture to remove", Toast.LENGTH_SHORT).show();
         }
     }
-
+    /**
+     * Saves the selected image to internal storage and returns the absolute path of the saved image file.
+     *
+     * @param uri The URI of the selected image.
+     * @return The absolute path of the saved image file, or null if saving fails.
+     */
     private String saveImageToInternalStorage(Uri uri) {
         try {
+            // Open an InputStream to read the selected image
             InputStream inputStream = getContentResolver().openInputStream(uri);
+            // Decode the input stream into a Bitmap
             Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
             File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
             File imageFile = File.createTempFile("profile_", ".jpg", storageDir);
             FileOutputStream out = new FileOutputStream(imageFile);
+            // Compress and write the bitmap data to the FileOutputStream
             bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
             out.close();
+            // Return the absolute path of the saved image file
             return imageFile.getAbsolutePath();
         } catch (IOException e) {
+            // If an IOException occurs, display a toast message indicating that image saving failed
             Toast.makeText(this, "Failed to save image", Toast.LENGTH_SHORT).show();
             return null;
         }
     }
+    /**
+     * Converts a Bitmap image to a Base64-encoded string.
+     *
+     * @param bitmap The Bitmap image to be converted.
+     * @return The Base64-encoded string representation of the Bitmap image.
+     */
     public String BitMapToString(Bitmap bitmap) {
+        // Create a ByteArrayOutputStream to hold the compressed bitmap data
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        // Compress the bitmap image to JPEG format with maximum quality (100), and write the compressed data to the ByteArrayOutputStream
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
         byte[] byteArray = stream.toByteArray();
         String result = Base64.encodeToString(byteArray, Base64.DEFAULT);
@@ -220,6 +299,13 @@ public class AttendeeProfileUpdate extends AppCompatActivity {
 
     }
     //https://www.geeksforgeeks.org/sha-256-hash-in-java/
+    /**
+     * Calculates the SHA-256 hash of the input string.
+     *
+     * @param input The input string to be hashed.
+     * @return The SHA-256 hash of the input string as a byte array.
+     * @throws NoSuchAlgorithmException If the SHA-256 algorithm is not available.
+     */
     public static byte[] getSHA(String input) throws NoSuchAlgorithmException
     {
         // Static getInstance method is called with hashing SHA
@@ -230,7 +316,12 @@ public class AttendeeProfileUpdate extends AppCompatActivity {
         // and return array of byte
         return md.digest(input.getBytes(StandardCharsets.UTF_8));
     }
-
+    /**
+     * Converts a byte array to a hexadecimal string representation.
+     *
+     * @param hash The byte array to be converted.
+     * @return The hexadecimal string representation of the byte array.
+     */
     public static String toHexString(byte[] hash)
     {
         // Convert byte array into signum representation
@@ -247,20 +338,30 @@ public class AttendeeProfileUpdate extends AppCompatActivity {
 
         return hexString.toString();
     }
+    /**
+     * Creates a bitmap image based on the provided string using SHA-256 hashing algorithm.
+     *
+     * @param string The input string to be hashed and used to generate the bitmap.
+     * @return The generated bitmap image.
+     */
     private Bitmap createBitmap(String string) {
+        // Create a new bitmap with dimensions 16x16 pixels and RGB_565 configuration
         Bitmap Invitebitmap = Bitmap.createBitmap(16, 16, Bitmap.Config.RGB_565);
         try {
+            // Get the SHA-256 hash of the input string
             byteList = getSHA(string);
         } catch (NoSuchAlgorithmException e) {
+            // Throw a runtime exception if NoSuchAlgorithmException occurs
             throw new RuntimeException(e);
         }
+        // Generate color lists based on the hash values
         for (int i = 0 ;i<16;i++) {
             finalColorList1[i] = colorlist.get(Math.abs(byteList[i]%10));
         }
         for (int i = 16 ;i<32;i++) {
             finalColorList2[i-16] = colorlist.get(Math.abs(byteList[i]%10));
         }
-
+        // Populate the bitmap with colors based on the generated color lists
         for (int x = 0; x < 16; x++) {
             for (int y = 0; y <16; y++) {
                 if (x%2 == 0) {
@@ -271,12 +372,22 @@ public class AttendeeProfileUpdate extends AppCompatActivity {
                 }
             }
         }
+        // Return the generated bitmap
         return Invitebitmap;
     }
+    /**
+     * Converts a Bitmap image to a Base64 encoded string.
+     *
+     * @param bitmap The Bitmap image to be converted.
+     * @return The Base64 encoded string representing the Bitmap image.
+     */
     private String bitmapToBase64(Bitmap bitmap) {
+        // Create a ByteArrayOutputStream to write the compressed bitmap data
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        // Compress the bitmap image to PNG format with 100% quality and write to the ByteArrayOutputStream
         bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
         byte[] byteArray = byteArrayOutputStream.toByteArray();
+        // Encode the byte array to Base64 string using default Base64 encoding
         return Base64.encodeToString(byteArray, Base64.DEFAULT);
     }
 }
--- app/src/main/java/com/example/thesix/AttendeePromoActivity.java
+++ app/src/main/java/com/example/thesix/AttendeePromoActivity.java
@@ -22,10 +22,17 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 
+import org.checkerframework.common.returnsreceiver.qual.This;
+
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.List;
 
+/**
+ * AttendeePromoActivity displays promotional information about an event to attendees,
+ * including the event name, description, and poster image. Attendees can view event details
+ * and sign up for the event using this activity.
+ */
 
 public class AttendeePromoActivity extends AppCompatActivity {
     private TextView eventName;
@@ -38,46 +45,77 @@ public class AttendeePromoActivity extends AppCompatActivity {
     public String organizerID;
     List<String> signUpIDList;
 
+    /**
+     * Initializes the activity, sets up UI components, retrieves event information from the intent,
+     * and displays the event details.
+     *
+     * @param savedInstanceState The saved instance state bundle.
+     */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.attendee_promo_activity);
-        eventName = findViewById(R.id.eventName);
-        eventDescription = findViewById(R.id.eventDescription);
-        eventPoster = findViewById(R.id.eventPoster);
-        backButton = findViewById(R.id.backButton);
+
+        eventName = findViewById(R.id.eventName); // TextView for displaying the event name
+        eventDescription = findViewById(R.id.eventDescription); // TextView for displaying the event description
+        eventPoster = findViewById(R.id.eventPoster); // ImageView for displaying the event poster image
+        backButton = findViewById(R.id.backButton); //Button for navigating back to the main activity
         signUpButton = findViewById(R.id.signUpButton);
+
+        // Firestore database helper
         firestoreHelper = new QrCodeDB();
+
+        // Event information
         Bundle bundle = getIntent().getExtras();
         String name = bundle.getString("name");
         String description = bundle.getString("description");
         String imageData = bundle.getString("imageData");
+
         organizerID = bundle.getString("organizerID");
         eventNum = bundle.getLong("eventNum");
         Log.d("showing",name+description);
+
         eventName.setText(name);
         eventDescription.setText(description);
         Bitmap bitmap = StringToBitMap(imageData);
         eventPoster.setImageBitmap(bitmap);
+
+        //button to take back to the beginning
         backButton.setOnClickListener(new View.OnClickListener() {
+            /** take back to attendee main
+             * @param v The view that was clicked.
+             */
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(AttendeePromoActivity.this, AttendeeMainActivity.class));
             }
         });
         signUpButton.setOnClickListener(new View.OnClickListener() {
+            /** take to signUp button
+             * @param v The view that was clicked.
+             */
             @Override
             public void onClick(View v) {
                 signUp(new SignUpCallback() {
+                    /**
+                     * Callback method invoked when the sign-up operation is completed.
+                     * This method updates the sign-up list with the device ID if the device
+                     * is not already signed up for the event.
+                     *
+                     * @param signUpIDList The list of device IDs that have signed up for the event.
+                     */
                     @Override
                     public void onSignUpCallback(List<String> signUpIDList) {
+                        // Get the device ID
                         String deviceID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
                         if (signUpIDList.contains(deviceID)) {
-
+                            // Check if the device ID is already in the sign-up list
                         }
                         else {
                             signUpIDList.add(deviceID);
                         }
+
+                        // Update the sign-up list in Firestore
                         firestoreHelper.getDeviceDocRef(organizerID).collection("event").document(String.valueOf(eventNum))
                                 .update("signUpIDList", signUpIDList).addOnSuccessListener(new OnSuccessListener<Void>() {
                                     @Override
@@ -100,9 +138,20 @@ public class AttendeePromoActivity extends AppCompatActivity {
     }
     private interface SignUpCallback {
 
+        /**
+         * Callback method invoked when the sign-up operation is completed.
+         * This method receives the list of device IDs that have signed up for the event.
+         * @param signUpIDList list of device id
+         */
         void onSignUpCallback(List<String> signUpIDList);
     }
+    /**
+     * Signs up the current device for the event.
+     *
+     * @param signUpCallback The callback interface for sign-up operations.
+     */
     private void signUp(SignUpCallback signUpCallback) {
+        // Retrieve the document reference for the event from Firestore
         firestoreHelper.getDeviceDocRef(organizerID).collection("event")
                 .document(String.valueOf(eventNum)).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                     @Override
@@ -110,8 +159,10 @@ public class AttendeePromoActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             DocumentSnapshot document = task.getResult();
                             if (document.exists()) {
+                                // Get the list of device IDs that have signed up for the event
                                 signUpIDList = (List<String>) document.get("signUpIDList");
                                 signUpCallback.onSignUpCallback(signUpIDList);
+                                // Invoke the callback method with the sign-up list
                                 Log.d("signup", "DocumentSnapshot data: " + document.getData());
                             } else {
                                 Log.d("signup", "No such document");
@@ -122,14 +173,24 @@ public class AttendeePromoActivity extends AppCompatActivity {
                     }
                 });
     }
+    /**
+     * Converts a Base64 encoded string to a Bitmap image.
+     *
+     * @param image The Base64 encoded string representing the image.
+     * @return The Bitmap image decoded from the Base64 string, or null if decoding fails.
+     */
     public Bitmap StringToBitMap(String image){
         try{
+            // Decode the Base64 string to byte array
             byte [] encodeByte= Base64.decode(image,Base64.DEFAULT);
 
+            // Convert the byte array to an InputStream
             InputStream inputStream  = new ByteArrayInputStream(encodeByte);
+            // Return the decoded Bitmap
             Bitmap bitmap  = BitmapFactory.decodeStream(inputStream);
             return bitmap;
         }catch(Exception e){
+            // Print error message if decoding fails
             e.getMessage();
             return null;
         }
