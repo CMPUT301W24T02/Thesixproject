b2a7d2515e9b544020f06fddea9433291222eb9a
D-e-elle <84299963+D-e-elle@users.noreply.github.com>
2024-04-05 19:45:47 -0600

    Removes Image from event listing Adapter and profile listing Adapter
    No removal from firestore, must compelte firestoreupdate

--- app/src/main/java/com/example/thesix/AdminImagesActivity.java
+++ app/src/main/java/com/example/thesix/AdminImagesActivity.java
@@ -7,7 +7,9 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -21,14 +23,26 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
+
+/**
+ * AdminImagesActivity class
+ * Requests necessary permissions and sets up the layout for Image listings.
+ * Lists all Images in the listview created in the app.
+ *
+ * Continue JavaDocs
+ *
+ */
 
 public class AdminImagesActivity extends AppCompatActivity {
     private Button back2AdminButton;
     private FirebaseFirestore firestore;
     private CollectionReference eventImagesRef;
     private CollectionReference profileImagesRef;
+    private ArrayList<Long> eventNumList;
+    private ArrayList<String> deviceIdList;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -39,14 +53,18 @@ public class AdminImagesActivity extends AppCompatActivity {
         ListView eventImageListView = findViewById(R.id.images_event_view);
         ListView profileImageListView = findViewById(R.id.images_profile_view);
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
-
         firestore = FirebaseFirestore.getInstance();
         eventImagesRef = firestore.collection("inviteQrCodes");
         profileImagesRef = firestore.collection("AttendeeProfileDB");
 
+        // For Event
         ArrayList<Bitmap> eventImageDataList = new ArrayList<>();
-        ArrayList<Bitmap> profileImageDataList = new ArrayList<>();
+        eventNumList = new ArrayList<>();
         CustomImageAdapter eventImageAdapter = new CustomImageAdapter(AdminImagesActivity.this, eventImageDataList);
+
+        // For Profile
+        ArrayList<Bitmap> profileImageDataList = new ArrayList<>();
+        deviceIdList = new ArrayList<>();
         CustomImageAdapter profileImageAdapter = new CustomImageAdapter(AdminImagesActivity.this, profileImageDataList);
 
         // Set Adapters
@@ -57,7 +75,6 @@ public class AdminImagesActivity extends AppCompatActivity {
         readEventData(new MyCallback() {
             @Override
             public void onCallback(List<String> list1, ArrayList<Bitmap> imageDataList, CustomImageAdapter imageAdapter) {
-                // Convert Base64 strings to Bitmaps and add to eventImageDataList
                 for (String base64String : list1) {
                     Bitmap bitmap = decodeBase64(base64String);
                     if (bitmap != null) {
@@ -72,7 +89,6 @@ public class AdminImagesActivity extends AppCompatActivity {
         readProfileData(new MyCallback() {
             @Override
             public void onCallback(List<String> list1, ArrayList<Bitmap> imageDataList, CustomImageAdapter imageAdapter) {
-                // Convert Base64 strings to Bitmaps
                 for (String base64String : list1) {
                     Bitmap bitmap = decodeBase64(base64String);
                     if (bitmap != null) {
@@ -82,6 +98,35 @@ public class AdminImagesActivity extends AppCompatActivity {
                 imageAdapter.notifyDataSetChanged();
             }
         }, profileImageDataList, profileImageAdapter);
+
+        eventImageListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                // Replace Here
+                Bitmap profilePosterBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.event_poster);
+                eventImageDataList.set(position, profilePosterBitmap);
+                eventImageAdapter.notifyDataSetChanged();
+
+                long eventNum = eventNumList.get(position);
+                eventFirestoreUpdate(eventNum, profilePosterBitmap);
+                return true;
+            }
+        });
+
+        profileImageListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                // Replace Here
+                Bitmap eventPosterBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.event_poster);
+                profileImageDataList.set(position, eventPosterBitmap);
+                profileImageAdapter.notifyDataSetChanged();
+
+                String deviceId = deviceIdList.get(position);
+                profileFirestoreUpdate(deviceId, eventPosterBitmap);
+
+                return true;
+            }
+        });
 
         back2AdminButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -97,9 +142,12 @@ public class AdminImagesActivity extends AppCompatActivity {
             @Override
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
                 List<String> base64Strings = new ArrayList<>();
+                eventNumList.clear();
                 for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
                     String base64String = document.getString("eventImageData");
+                    Long eventNum = document.getLong("eventNum");
                     base64Strings.add(base64String);
+                    eventNumList.add(eventNum);
                 }
                 myCallback.onCallback(base64Strings, imageDataList, imageAdapter);
             }
@@ -111,10 +159,15 @@ public class AdminImagesActivity extends AppCompatActivity {
         profileImagesRef.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                String TAG = "button";
                 List<String> base64Strings = new ArrayList<>();
+                deviceIdList.clear();
+
                 for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
                     String base64String = document.getString("profile_image");
+                    deviceIdList.add(document.getId());
                     base64Strings.add(base64String);
+                    Log.d(TAG, "docIds Added"+ deviceIdList);
                 }
                 myCallback.onCallback(base64Strings, imageDataList, imageAdapter);
             }
@@ -124,9 +177,35 @@ public class AdminImagesActivity extends AppCompatActivity {
     public interface MyCallback {
         void onCallback(List<String> list1, ArrayList<Bitmap> imageDataList, CustomImageAdapter imageAdapter);
     }
-
+    // CHatGPt: Prompt, bitmap to string, string to bitmap
     private Bitmap decodeBase64(String base64String) {
         byte[] decodedBytes = Base64.decode(base64String, Base64.DEFAULT);
         return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+    }
+
+    public String bitmapToBase64(Bitmap bitmap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        byte[] byteArray = baos.toByteArray();
+        String base64String = Base64.encodeToString(byteArray, Base64.DEFAULT);
+        return base64String;
+    }
+
+    private void eventFirestoreUpdate(long eventNum, Bitmap eventPosterBitmap) {
+        String base64Image = bitmapToBase64(eventPosterBitmap);
+        String TAG = "button";
+
+        eventImagesRef.whereEqualTo("eventNum", eventNum)
+                .get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    Log.d(TAG, "SuccessListener");
+                    for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                        documentSnapshot.getReference().update("eventImageData", base64Image);
+                        Log.d(TAG, "eventImageData" + documentSnapshot.getReference());
+                    }
+                });
+    }
+
+    private void profileFirestoreUpdate(String deviceId, Bitmap eventPosterBitmap) {
     }
 }
