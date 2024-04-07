a1b862ba61aa15e2713307320203ec3ccc22b9b2
AlexGuo7060 <158379323+AlexGuo7060@users.noreply.github.com>
2024-04-07 03:20:44 -0600

    Update OrganizerMainActivity.java

--- app/src/main/java/com/example/thesix/OrganizerMainActivity.java
+++ app/src/main/java/com/example/thesix/OrganizerMainActivity.java
@@ -51,11 +51,11 @@ public class OrganizerMainActivity extends AppCompatActivity {
     private String name;
 
 
-    /**
-     *Creating eventbutton
-     @param :
-     @return void
-     **/
+    /** Creating Organizer Main Activity Page
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down then this Bundle contains the data it most
+     *                           recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
