5f1fe48703b8a1eb76dfe98d5e497508ee2485d8
Arjun <arjun10@ualberta.ca>
2024-04-08 01:13:40 -0600

    Added code for guestlist and guest count

--- app/src/main/java/com/example/thesix/MainActivity.java
+++ app/src/main/java/com/example/thesix/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d(deviceID, "this is device id ");
         Log.d("DeviceId", deviceID);
 
-        adminId = deviceID;
+        //adminId = deviceID;
         //27150c669e8b1dc4
 
         if (deviceID.equalsIgnoreCase(adminId)) {
